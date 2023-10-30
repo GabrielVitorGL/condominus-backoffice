97c68c057ecaec5b4654e806ffbce1c06ab4bd63
Gabriel Vitor <gabriel.lourenco18@etec.sp.gov.br>
2023-10-30 10:49:42 -0300

    fixing morador page

--- app/backoffice/page.tsx
+++ app/backoffice/page.tsx
@@ -1,10 +1,14 @@
 "use client"
 import { Admin, Resource, ListGuesser, EditGuesser, ShowGuesser } from 'react-admin';
+import MoradorList from './Moradores/MoradorList';
 
 export default function Backoffice() {
   return (
     <Admin>
-+   <Resource name="pessoas" list={ListGuesser} />
++   <Resource name="/pessoas" list={ListGuesser} />
+<Resource name="/moradores" list={MoradorList} />
+<Resource name="/aa" list={ListGuesser} />
+<Resource name="/asa" list={ListGuesser} />
   </Admin>
   );
 }
